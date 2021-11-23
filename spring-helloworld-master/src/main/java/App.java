dc99ad3c9c1fcc874bf6f13e314e27e87bf86f93
Nurdas <63863660+nurdas00@users.noreply.github.com>
2021-11-23 21:10:43 +0300

    Add files via upload

---
+++ spring-helloworld-master/src/main/java/App.java
@@ -0,0 +1,20 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean2);
+        System.out.println(catBean1 == catBean2);
+
+        System.out.println(bean.getMessage());
+        System.out.println(catBean1.getMeow());
+    }
+}
---
+++ spring-helloworld-master/src/main/java/Cat.java
@@ -0,0 +1,7 @@
+public class Cat{
+    String meow;
+
+    public String getMeow(){return meow;}
+
+    public void setMeow(String meow){this.meow = meow;}
+}
